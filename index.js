839186d1f247018030bbd7b3515f7b2621e6f8e6
Bruno305Rodrigues <bruno_lmr_op@hotmail.com>
2020-11-07 11:32:02 -0300

    Adicionando descontos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const vagas = { 
+const vagas = {
     a1: null,
     a2: null,
     b1: null,
@@ -10,82 +10,94 @@ const vagas = {
 function entradaDeCarro(carro) {
     var estaTudoCheio = true;
 
-    for(vaga in vagas) {
-        if(vagas[vaga] === null) {
+    for (vaga in vagas) {
+        if (vagas[vaga] === null) {
             vagas[vaga] = carro;
             estaTudoCheio = false;
             break;
         }
     }
-    if(estaTudoCheio) {
+    if (estaTudoCheio) {
         console.log("Estacionamento cheio")
     }
 }
 
-function saidaDeCarro(placa){
-    for(vaga in vagas){
-        if(vagas[vaga] !== null && vagas[vaga].placa === placa) {
+function saidaDeCarro(placa, tempo,cor,ano) {
+    for (vaga in vagas) {
+        if (vagas[vaga] !== null && vagas[vaga].placa === placa) {
             vagas[vaga] = null;
+            console.log("Total a pagar:", tempoDePermanecia(tempo) - ((tempoDePermanecia(tempo)* descontoAnodoCarro(ano))/100) - descontoCor(cor));   //descontoCor(cor)  );
             break;
         }
     }
 
 }
 
-function pesquisaCarro(nomeVaga){
+function consultaVaga(nomeVaga) {
 
-        console.log(vagas[nomeVaga]);
+    for (vaga in vagas) {
+
+        if (vaga === nomeVaga) {
+            if (vagas[vaga] === null) {
+                console.log("Esta vaga esta disponível");
+                break;
+
+            }
+            console.log("Modelo:", vagas[vaga].modelo);
+
+        }
+    }
 }
 
-function pesquisaPorPlaca(placa){
+function pesquisaPorPlaca(placa) {
     var carroEncontrado = false;
-    for( vaga in vagas){
-        if(vagas[vaga] !== null && vagas[vaga].placa === placa){
-            console.log("Vaga:" , vaga);
-            console.log("Modelo:" , vagas[vaga].modelo);
-            console.log("Placa:" , vagas[vaga].placa);
-            console.log("Cor:" , vagas[vaga].cor);
+    for (vaga in vagas) {
+        if (vagas[vaga] !== null && vagas[vaga].placa === placa) {
+            console.log("Vaga:", vaga);
+            console.log("Modelo:", vagas[vaga].modelo);
+            console.log("Placa:", vagas[vaga].placa);
+            console.log("Cor:", vagas[vaga].cor);
             console.log("--------------------")
 
 
             carroEncontrado = true;
-           break;
+            break;
         }
-        
+
 
     }
-    if(!carroEncontrado){
+    if (!carroEncontrado) {
         console.log("Veículo não encontrado.")
     }
 }
 
 
-function pesquisarPorModelo(modelo){
+function pesquisarPorModelo(modelo) {
     var carroEncontrado = false;
-    for( vaga in vagas){
-        if(vagas[vaga] !== null && vagas[vaga].modelo === modelo){
+    for (vaga in vagas) {
+        if (vagas[vaga] !== null && vagas[vaga].modelo === modelo) {
             console.log("Modelo encontrado na vaga ", vaga);
             console.log("--------------------")
             carroEncontrado = true
-            
+
         }
     }
-    if(!carroEncontrado){
+    if (!carroEncontrado) {
         console.log("Modelo não encontrado.")
     }
 }
 
-function pesquisarPorCor(cor){
+function pesquisarPorCor(cor) {
     var carroEncontrado = false;
-    for( vaga in vagas){
-        if(vagas[vaga] !== null && vagas[vaga].cor === cor){
+    for (vaga in vagas) {
+        if (vagas[vaga] !== null && vagas[vaga].cor === cor) {
             console.log("Cor encontrada na vaga ", vaga);
             console.log("--------------------")
             carroEncontrado = true
-            
+
         }
     }
-    if(!carroEncontrado){
+    if (!carroEncontrado) {
         console.log("Cor não encontrada.")
     }
 }
@@ -93,25 +105,59 @@ function pesquisarPorCor(cor){
 
 
 
+function tempoDePermanecia(tempo) {
+    var valor = (tempo * 100 / 60)
+    return valor.toFixed(2);
+}
+
+function descontoCor(cor) {
+    if (cor === "Azul") {
+        return 10;
+    }else if(cor === "Branco"){
+        return 15;
+
+    }else if(cor === "Preto"){
+        return 20;
+    }else{
+        return 0;
+    }
 
 
-entradaDeCarro({modelo: "Fusca", ano: 1973, placa: 1, cor: "azul"});
-entradaDeCarro({modelo: "Monza", ano: 1975, placa: 2, cor: "preto"});
-entradaDeCarro({modelo: "Corsa", ano: 1973, placa: 3, cor: "azul"});
-entradaDeCarro({modelo: "Ferrari", ano: 1975, placa:4, cor: "preto"});
-entradaDeCarro({modelo: "Corsa", ano: 1973, placa:5, cor: "azul"});
-entradaDeCarro({modelo: "Corolla", ano: 1975, placa: 6, cor: "preto"});
+
+}
+
+function descontoAnodoCarro(ano){
+    if(ano >=2010 && ano <= 2015 ){
+        return 15;
+    }else if(ano >=2000 && ano < 210){
+        return 20;
+    }else if(ano < 2000){
+        return 30;
+    }else{
+        return 0;
+    }
+}
+
+
+
+
+
+
+entradaDeCarro({ modelo: "Fusca", ano: 1973, placa: 1, cor: "azul" });
+entradaDeCarro({ modelo: "Monza", ano: 1975, placa: 2, cor: "preto" });
+entradaDeCarro({ modelo: "Corsa", ano: 1973, placa: 3, cor: "azul" });
+entradaDeCarro({ modelo: "Ferrari", ano: 1975, placa: 4, cor: "preto" });
+entradaDeCarro({ modelo: "Corsa", ano: 1973, placa: 5, cor: "azul" });
+entradaDeCarro({ modelo: "Corolla", ano: 1975, placa: 6, cor: "preto" });
 
 //entradaDeCarro({modelo: "Chevette", ano: 1975, placa: 2, cor: "preto"});
 //entradaDeCarro({modelo: "Camaro", ano: 2018, placa: 2, cor: "prata"});
 //saidaDeCarro(2);
-saidaDeCarro(4);
-saidaDeCarro(2);
-entradaDeCarro({modelo: "Camaro", ano: 2018, placa: 2, cor: "prata"});
+//saidaDeCarro(4);
+entradaDeCarro({ modelo: "Camaro", ano: 2018, placa: 2, cor: "prata" });
 
 
 
-console.log(vagas);
 //pesquisaCarro("c1");
 pesquisaPorPlaca(1);
 pesquisaPorPlaca(5);
@@ -120,3 +166,16 @@ console.log("-------------");
 
 pesquisarPorModelo("Corsa");
 pesquisarPorCor("azul");
+
+console.log("-------------");
+
+consultaVaga("b2");
+
+saidaDeCarro(2, 120, "Azul", 1975);
+console.log(vagas);
+;
+
+
+
+
+
